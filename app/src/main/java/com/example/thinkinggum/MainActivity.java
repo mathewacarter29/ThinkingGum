5b11dcf96f059a92008edfa56900b592c8680aee
mathewacarter29 <mathewacarter29@vt.edu>
2021-07-17 18:46:22 -0400

    Deleted "Random Topic" button as that will be implemented in V2. Created pop-up window when topic is selected.

--- app/src/main/java/com/example/thinkinggum/MainActivity.java
+++ app/src/main/java/com/example/thinkinggum/MainActivity.java
@@ -9,25 +9,18 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button randomBut, viewBut;
+    Button viewBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        randomBut = (Button) findViewById(R.id.randomBut);
         viewBut = (Button) findViewById(R.id.viewBut);
     }
 
     public void onClick(View view) {
-        int id = view.getId();
-        if (id == randomBut.getId()) {
-            //Implement when all topics are added
-        }
-        else {
-            Intent intent = new Intent(this, TopicActivity.class);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, TopicActivity.class);
+        startActivity(intent);
     }
 }
